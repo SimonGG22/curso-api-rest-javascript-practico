b3f0eb5e5f9676b8f668a48592dd9eb96c439e44
Simon Guerrero <simonguerreroguerrero@gmail.com>
2023-02-21 21:48:55 -0500

    bug fix duplicate load

--- src/main.js
+++ src/main.js
@@ -12,9 +12,9 @@ async function getTrendingMoviesPreview () {
     const {data} = await api ('trending/movie/day');
     const movies = data.results;
 
-    movies.forEach(movie => {
-        const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    trendingMoviesPreviewList.innerHTML = " ";
 
+    movies.forEach(movie => {
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
 
@@ -30,10 +30,11 @@ async function getTrendingMoviesPreview () {
 
 async function getCategoriesPreview () {
     const {data} = await api ('genre/movie/list');
-
     const categories = data.genres;
+
+    categoriesPreviewList.innerHTML = " ";
+
     categories.forEach(category => {
-        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
 
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container');
@@ -48,6 +49,3 @@ async function getCategoriesPreview () {
         categoriesPreviewList.appendChild(categoryContainer)
     });
 }
-
-
-
